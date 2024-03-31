620ce1f4f367596e912e006aceeefb58f633b8ba
AlidiPatrick <alidipatrick@gmail.com>
2024-03-31 07:25:54 +0000

    chap 8 done, new models for property type, tag and offer, xml menus updated

--- models/estate_property.py
+++ models/estate_property.py
@@ -5,6 +5,7 @@ class EstateProperty(models.Model):
     _description = "Estate Property"
 
     name = fields.Char('Title', required=True)
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
     date_availability = fields.Date('Available From', copy=False, default=tools.date_utils.add(fields.Date.today(), months=3))
@@ -19,3 +20,7 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection([('north', 'North'), (['south', 'South']), ('east', 'East'), ('west', 'West')], string='Garden Orientation')
     active = fields.Boolean(default=True)
     state = fields.Selection([('new','New'), ('offer_received','Offer Received'), ('offer_accepted','Offer Accepted'), ('sold','Sold'), ('canceled','Canceled')], required=True, default='new')
+    salesperson = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.uid)
+    buyer = fields.Many2one('res.partner', string='Buyer', copy=False)
+    tag_ids = fields.Many2many('estate.property.tag', string="Tags")
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
